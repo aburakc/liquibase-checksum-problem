61cefcbf9a183fbdd48a8335d8920af860e86bb4
aburakc <aburakc@yahoo.com>
2024-02-13 20:42:04 +0300

    first commit

---
+++ src/main/java/com/aburakc/liquibase/checksum/LiquibaseChecksumTestApplication.java
@@ -0,0 +1,13 @@
+package com.aburakc.liquibase.checksum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseChecksumTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseChecksumTestApplication.class, args);
+	}
+
+}
